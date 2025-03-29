10d355efbbe2b65c803bf3f9038d9ddf3cac28b1
lendrik-kumar <sanketkamboj2006@gmail.com>
2025-03-29 22:53:31 +0530

    Initial commit

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const ws_1 = require("ws");
-const wss = new ws_1.WebSocketServer({ port: 8080 });
+const port = process.env.PORT || 8080; // Use the PORT environment variable or default to 8080
+const wss = new ws_1.WebSocketServer({ port });
 let userCount = 0;
 let allSockets = [];
 wss.on("connection", (socket) => {
